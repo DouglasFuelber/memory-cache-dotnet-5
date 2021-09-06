da956091675d57ab48f66b2fa0f3bdd943b8a14a
Douglas Fuelber <douglas.fuelber@tekann.com.br>
2021-09-06 11:56:34 -0300

    Created project and banks API request

---
+++ MemoryCache.API/Controllers/BanksController.cs
@@ -0,0 +1,30 @@
+﻿using MemoryCache.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MemoryCache.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BanksController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string brazilBanksApiUrl = "https://brasilapi.com.br/api/banks/v1";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(brazilBanksApiUrl);
+                var responseData = await response.Content.ReadAsStringAsync();
+
+                var banks = JsonSerializer.Deserialize<List<Bank>>(responseData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                return Ok(banks);
+            }
+        }
+    }
+}
---
+++ MemoryCache.API/Models/Bank.cs
@@ -0,0 +1,10 @@
+﻿namespace MemoryCache.API.Models
+{
+    public class Bank
+    {
+        public string ispb { get; set; }
+        public string name { get; set; }
+        public int? code { get; set; }
+        public string fullName { get; set; }
+    }
+}
